bcb6e269dcadbb4df1b5c5f54be79dc74720cd9b
Daishikofy <48716129+Daishikofy@users.noreply.github.com>
2019-08-26 05:46:10 -0300

    Add files via upload
    The use of the tag "Ground" enables the player to move freely but any contact with a "Ground" platform even if from the sides or from below will result in the player not being onGround anymore.

---
+++ PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float jumpHeight;
+
+    private Rigidbody2D rigidbody;
+    private Collider2D collider;
+    private Vector2 playerMovement;
+    [SerializeField]
+    private bool onGround = false;
+    private bool isJumping;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        float horizontalMovement = horizontalInput * speed;
+        playerMovement.x = horizontalMovement;
+
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
+            Jump();
+            isJumping = true;
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        if (onGround && !isJumping)
+            rigidbody.MovePosition(rigidbody.position + playerMovement * Time.deltaTime);      
+        else
+        {
+            if (isJumping)
+                isJumping = false;
+            rigidbody.AddForce(playerMovement);
+            Debug.Log("Player movement : " + playerMovement);
+        }
+            
+    }
+
+    private void Jump()
+    {
+        Vector2 impulse = new Vector2(0, jumpHeight);
+        rigidbody.AddForce(impulse, ForceMode2D.Impulse);
+        Debug.Log("WHEEE");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ColliderDistance2D distance = collision.collider.Distance(collider);
+        if (collision.transform.CompareTag("Ground"))
+            onGround = true;     
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        ColliderDistance2D distance = collision.collider.Distance(collider);
+        if (collision.transform.CompareTag("Ground"))
+            onGround = false;
+    }
+}
+
