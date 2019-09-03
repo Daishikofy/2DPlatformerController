6def557afe4e526085154908ae83cf055543c57a
Daishikofy <48716129+Daishikofy@users.noreply.github.com>
2019-09-03 06:43:44 -0300

    Added extra jumps
    Added extra jumps and removed isJumping variable

--- PlayerController_2.cs
+++ PlayerController_2.cs
@@ -8,19 +8,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float jumpHeight;
     [SerializeField]
-    private float airSpeed;
+    private int extraJumps;
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float airReactivity;
 
     private Rigidbody2D rigidbody2d;
     private Collider2D collider2d;
 
     private Vector2 playerMovement;
     private bool onGround = false;
-    private bool isJumping;
+    private int remainingJumps;
 
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         collider2d = GetComponent<Collider2D>();
+        remainingJumps = extraJumps;
     }
 
     void Update()
@@ -38,10 +42,15 @@ public class PlayerController : MonoBehaviour
            * The velocity in y is set to "jumpHeight" during the frame of the jump
            * It decreases under the force of the gravity
          */
-        if (Input.GetButtonDown("Jump") && onGround)
+        if (Input.GetButtonDown("Jump"))
         {
-            isJumping = true;
-            Jump();
+            if (onGround)
+                Jump();
+            else if (remainingJumps > 0)
+            {
+                remainingJumps--;
+                Jump();
+            }
         }
     }
 
@@ -51,12 +60,9 @@ public class PlayerController : MonoBehaviour
            * If the character is in the air, lerp is used to soften it's movements and give and inertia sensation
            * The velocity in y is always set to be the current y velocity
         */
-        if (!onGround || isJumping)
-        {
-            if (isJumping)
-                isJumping = false;
-            playerMovement.x = Mathf.Lerp(rigidbody2d.velocity.x, playerMovement.x, airSpeed);
-        }
+        if (!onGround)
+            playerMovement.x = Mathf.Lerp(rigidbody2d.velocity.x, playerMovement.x, airReactivity);
+
         playerMovement.y = rigidbody2d.velocity.y;
         rigidbody2d.velocity = playerMovement;
     }
@@ -72,6 +78,7 @@ public class PlayerController : MonoBehaviour
         ColliderDistance2D distance = collision.collider.Distance(collider2d);
         if (collision.transform.CompareTag("Ground") && Vector2.Angle(distance.normal, Vector2.up) < 45)
             onGround = true;
+        remainingJumps = extraJumps;
     }
 
     private void OnCollisionExit2D(Collision2D collision)
@@ -82,6 +89,8 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        rigidbody2d.velocity = Vector2.up * jumpHeight;
+        playerMovement.x = rigidbody2d.velocity.x;
+        playerMovement.y = jumpHeight;
+        rigidbody2d.velocity = playerMovement;
     }
 } /* End of class: PlayerController */
